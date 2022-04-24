793333f59a97a783d26cc1de2439d54eafea0880
Lishelun <lishelun@qq.com>
2022-04-24 10:52:21 +0800

    添加queue

--- app/web/controller/Test.php
+++ app/web/controller/Test.php
@@ -5,11 +5,19 @@ declare (strict_types=1);
 namespace app\web\controller;
 
 use hutphp\Controller;
+use hutphp\helper\TableHelper;
 
 class Test extends Controller
 {
     public function index()
     {
         return time();
+    }
+    public function table(){
+        $res=true;
+//        $table=TableHelper::instance()->setTable(dbtbpre().'tabletest');
+//        $res=$table->addIndex(['tag'],'','fulltext');
+
+        $this->success('test',[$res]);
     }
 }
