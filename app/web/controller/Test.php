0b566692c36d5c8c6d3e4c65e351fec45f92553c
Lishelun <lishelun@qq.com>
2022-04-27 21:42:48 +0800

    update

--- app/web/controller/Test.php
+++ app/web/controller/Test.php
@@ -1,11 +1,10 @@
 <?php
 
-declare (strict_types=1);
+declare (strict_types = 1);
 
 namespace app\web\controller;
 
 use hutphp\Controller;
-use hutphp\helper\TableHelper;
 
 class Test extends Controller
 {
@@ -13,11 +12,14 @@ class Test extends Controller
     {
         return time();
     }
-    public function table(){
-        $res=true;
-//        $table=TableHelper::instance()->setTable(dbtbpre().'tabletest');
-//        $res=$table->addIndex(['tag'],'','fulltext');
 
-        $this->success('test',[$res]);
+    public function table()
+    {
+
+    }
+
+    public function request()
+    {
+        var_dump($this->request->buildToken());
     }
 }
